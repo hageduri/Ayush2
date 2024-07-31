a0e104c6aba86ceaa18b91d8aef7c790faf56d2f
Chow Vishal Longchat <142925746+Chowvis@users.noreply.github.com>
2024-07-31 11:42:59 +0530

    migration
    Block,District and users. rest will be done by duri

---
+++ database/migrations/2024_07_30_101002_add_columns_to_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->string('nin', 50)->unique()->nullable();
+            $table->string('role', 50)->nullable();
+            $table->string('gender', 20)->nullable();
+            $table->date('dob')->nullable();
+            $table->string('bank_name', 100)->nullable();
+            $table->string('account_no', 20)->nullable();
+            $table->string('ifsc_code', 11)->nullable();
+            $table->text('address')->nullable();
+            $table->string('contact_1', 15)->nullable();
+            $table->string('contact_2', 15)->nullable();
+            $table->string('status', 20)->nullable();
+            $table->string('designation',255)->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
